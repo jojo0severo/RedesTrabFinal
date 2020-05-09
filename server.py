2969a95deeaa31c7eaa683345ac2d583c445b75f
jojo0severo <joaovbsevero@gmail.com>
2020-05-09 03:03:22 -0300

    Added run method for server

--- server.py
+++ server.py
@@ -4,3 +4,8 @@ from gevent.server import DatagramServer
 class MultiThreadServer(DatagramServer):
     def handle(self, data, address):
         self.socket.sendto(('Received %s bytes' % len(data)).encode('utf-8'), address)
+
+
+if __name__ == '__main__':
+    print('Receiving datagrams on 127.0.0.1:8080')
+    MultiThreadServer('127.0.0.1:8080').serve_forever()
