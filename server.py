a1d3e5613552da75aa2e14476117d78845e5d349
jojo0severo <joaovbsevero@gmail.com>
2020-05-09 01:21:29 -0300

    Added echo response for UDP testing

--- server.py
+++ server.py
@@ -3,4 +3,4 @@ from gevent.server import DatagramServer
 
 class MultiThreadServer(DatagramServer):
     def handle(self, data, address):
-        pass
+        self.socket.sendto(('Received %s bytes' % len(data)).encode('utf-8'), address)
