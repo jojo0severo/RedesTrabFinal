9033b80d478f8683e69c6dec6c71105b22baeaa0
jojo0severo <joaovbsevero@gmail.com>
2020-05-12 06:01:02 -0300

    Changed to a synchronous server where it is possible to ensure that the client is receiving the messages

--- server.py
+++ server.py
@@ -1,41 +1,158 @@
+import time
 import json
-from gevent.server import DatagramServer
+import socket
 from controller.json_transformer import JSONTransformer
 
 
-class MultiThreadServer(DatagramServer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class SocketServer:
+    def __init__(self, host='127.0.0.1', port=8080):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((host, port))
+        print(f'Listening into: "{host}:{port}"')
 
         self.transformer = JSONTransformer()
 
         self.event_handler = {
-            'connect': self.transformer.connect_user,
-            'getUsers': self.transformer.get_users,
-            'getSubjects': self.transformer.get_subjects,
-            'getGroups': self.transformer.get_groups,
-            'createGroup': self.transformer.create_group,
-            'joinGroup': self.transformer.join_group,
-            'leaveGroup': self.transformer.leave_group,
-            'startMatch': self.transformer.start_match,
-            'endMatch': self.transformer.end_match,
-            'ok': self._ok
+            'connect': self.connect_user,
+            'getSubjects': self.get_subjects,
+            'getGroups': self.get_groups,
+            'getUsers': self.get_users,
+            'createGroup': self.create_group,
+            'joinGroup': self.join_group,
+            'leaveGroup': self.leave_group,
+            'startMatch': self.start_match,
+            'endMatch': self.end_match
         }
 
+    def listen(self):
+        while True:
+            message, address = self.socket.recvfrom(4096)
+            self.handle(message.decode('utf-8').replace('\'', '"'), address)
+
     def handle(self, data, address):
-        json_received = json.loads(data.decode('utf-8'))
+        try:
+            json_received = json.loads(data)
 
-        event = json_received['event']
-        if event == 'connect':
-            json_received['address'] = address
+            event = json_received['event']
+            if event == 'connect':
+                json_received['json']['address'] = address
 
-        response = self.event_handler.get(event, default=self.transformer.unrecognized)(json_received['json'])
-        self.socket.sendto(json.dumps(response).encode('utf-8'), address)
+            if event not in self.event_handler:
+                self.unrecognized(address, event)
+            else:
+                self.event_handler.get(event)(address, json_received['json'])
 
-    def _ok(self, *args):
-        return {'result': True, 'status': 200, 'message': 'Ok', 'data': {}}
+        except json.JSONDecodeError as err:
+            self.json_decode_error(address, data, str(err))
+
+    def send(self, message, address):
+        self.socket.sendto(message, address)
+        now = time.time()
+        while time.time() - now < 5:
+            msg, addr = self.socket.recvfrom(4096)
+            msg = msg.decode('utf-8')
+            if addr == address and msg.lower() == 'ok':
+                self.socket.sendto('ok'.encode('utf-8'), address)
+                self.check_ok(address)
+                return
+
+        self.send(message, address)
+
+    def check_ok(self, address):
+        self.socket.sendto('ok'.encode('utf-8'), address)
+
+        now = time.time()
+        while time.time() - now < 5:
+            msg, addr = self.socket.recvfrom(4096)
+            msg = msg.decode('utf-8')
+            if addr == address and msg.lower() == 'ok':
+                return
+            else:
+                break
+
+        self.check_ok(address)
+
+    def connect_user(self, address, json_data):
+        response = self.transformer.connect_user(json_data)
+
+        str_resp = json.dumps(response).encode('utf-8')
+        self.send(str_resp, address)
+
+    def get_subjects(self, address, json_data):
+        response = self.transformer.get_subjects()
+
+        str_resp = json.dumps(response).encode('utf-8')
+        self.send(str_resp, address)
+
+    def get_groups(self, address, json_data):
+        response = self.transformer.get_groups(json_data)
+
+        str_resp = json.dumps(response).encode('utf-8')
+        self.send(str_resp, address)
+
+    def get_users(self, address, json_data):
+        response = self.transformer.get_users(json_data)
+
+        str_resp = json.dumps(response).encode('utf-8')
+        self.send(str_resp, address)
+
+    def create_group(self, address, json_data):
+        response = self.transformer.create_group(json_data)
+
+        str_resp = json.dumps(response).encode('utf-8')
+        self.send(str_resp, address)
+
+        if response['result']:
+            update_response, addresses = self.transformer.get_update_response('create_group', address, json_data)
+            str_update_resp = json.dumps(update_response).encode('utf-8')
+            for addr in addresses:
+                self.send(str_update_resp, addr)
+
+    def join_group(self, address, json_data):
+        response = self.transformer.join_group(json_data)
+
+        str_resp = json.dumps(response).encode('utf-8')
+        self.send(str_resp, address)
+
+        if response['result']:
+            update_response, addresses = self.transformer.get_update_response('join_group', address, response)
+            str_update_resp = json.dumps(update_response).encode('utf-8')
+            for addr in addresses:
+                self.send(str_update_resp, addr)
+
+    def leave_group(self, address, json_data):
+        response = self.transformer.leave_group(json_data)
+
+        str_resp = json.dumps(response).encode('utf-8')
+        self.send(str_resp, address)
+
+        if response['result']:
+            update_response, addresses = self.transformer.get_update_response('leave_group', address, response)
+            str_update_resp = json.dumps(update_response).encode('utf-8')
+            for addr in addresses:
+                self.send(str_update_resp, addr)
+
+    def start_match(self, address, json_data):
+        # str_resp = json.dumps(response).encode('utf-8')
+        # self.send(str_resp, address)
+        pass
+
+    def end_match(self, address, json_data):
+        # str_resp = json.dumps(response).encode('utf-8')
+        # self.send(str_resp, address)
+        pass
+
+    def unrecognized(self, address, event):
+        resp = self.transformer.unrecognized(event)
+        str_resp = json.dumps(resp).encode('utf-8')
+        self.send(str_resp, address)
+
+    def json_decode_error(self, address, json_data, error):
+        resp = self.transformer.json_decode_error(json_data, error)
+        str_resp = json.dumps(resp).encode('utf-8')
+        self.send(str_resp, address)
 
 
 if __name__ == '__main__':
-    print('Receiving datagrams on 127.0.0.1:8080')
-    MultiThreadServer('127.0.0.1:8080').serve_forever()
+    s = SocketServer()
+
